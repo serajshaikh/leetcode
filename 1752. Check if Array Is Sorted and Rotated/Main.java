cab905c351425e43b64577823ce03812b73dfc11
Seraj <serazshaikh07@gmail.com>
2025-02-02 22:40:23 +0530

    completed daily chlanges

---
+++ 1752. Check if Array Is Sorted and Rotated/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.check(new int[]{3, 4, 5, 1, 2})); // true
+        System.out.println(solution.check(new int[]{3, 4, 5, 1, 5})); // true
+    }
+}
