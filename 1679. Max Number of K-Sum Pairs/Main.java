b232e399e1027b3ff21f54568f582bc9c6d74378
Seraj <serazshaikh07@gmail.com>
2025-02-05 00:14:01 +0530

    1679: Max number of k-sum pairs

---
+++ 1679. Max Number of K-Sum Pairs/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxOperations(new int[]{1, 2, 3, 4}, 5));
+    }
+}
