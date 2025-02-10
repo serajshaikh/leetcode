2c784c4225c0f0e55182df7704c80c0cc5d89ce6
Seraj <serazshaikh07@gmail.com>
2025-02-10 22:26:25 +0530

    Equal Row and Column: done

--- java/2352. Equal Row and Column Pairs/Main.java
+++ java/2352. Equal Row and Column Pairs/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.
+        System.out.println(solution.equalPairs(new int[][]{{3,2,1},{1,7,6},{2,7,7}}));
     }
 }
--- java/2352. Equal Row and Column Pairs/Solution.java
+++ java/2352. Equal Row and Column Pairs/Solution.java
@@ -2,15 +2,28 @@ import java.util.*;
 
 public class Solution {
     public int equalPairs(int[][] grid) {
-        int r=grid.length;
-        int c=grid[0].length;
-        int count=0;
-        Map<String, Integer> freqMapper=new HashMap<>();
-        for(int[] k:grid){
-            String key=Arrays.toString(k);
-
+        int r = grid.length;
+        int c = grid[0].length;
+        int count = 0;
+        
+        Map<String, Integer> freqMapper = new HashMap<>();
+        
+        for (int[] row : grid) {
+            String key = Arrays.toString(row);
+            freqMapper.put(key, freqMapper.getOrDefault(key, 0) + 1);
         }
+        
+        for (int j = 0; j < c; j++) {
+            int[] column = new int[r];
+            for (int i = 0; i < r; i++) {
+                column[i] = grid[i][j];
+            }
+            String key = Arrays.toString(column);
+            if (freqMapper.containsKey(key)) {
+                count += freqMapper.get(key);
+            }
+        }
+        
         return count;
     }
-    
 }
